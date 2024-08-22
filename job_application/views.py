bcfe889c87737e9b26a76d0a9e3e9cabaa60fd43
pavankumarm2505 <pavankumarm2505@gmail.com>
2024-08-21 22:40:56 -0400

    added a success message along with storing the data in the sql database

--- job_application/views.py
+++ job_application/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .forms import ApplicationForms
-
+from .models import Form
+from django.contrib import messages
 
 def index(request):
     # this is if user presses the send/ submit button
@@ -12,6 +13,9 @@ def index(request):
             email = form.cleaned_data["email"]
             date = form.cleaned_data["date"]
             occupation = form.cleaned_data["occupation"]
-            print(first_name)
+
+            Form.objects.create(first_name=first_name, last_name= last_name, email=email, date=date,
+                               occupation=occupation)
+            messages.success(request, f"{first_name}, your form was successfully submitted")
 
     return render(request, "index.html")
