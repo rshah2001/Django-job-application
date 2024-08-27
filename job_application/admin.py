f92c745411439328613456f2da375d1c3c2de435
pavankumarm2505 <pavankumarm2505@gmail.com>
2024-08-27 11:19:50 -0400

    added admin page where admins can see the list of applicants, search their name, use filter for dates and occupation and occupation was set to read only and 10 per page

--- job_application/admin.py
+++ job_application/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Form
 
-# Register your models here.
+
+class FormAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "email","date", "occupation")
+    search_fields = ("first_name", "last_name", "email","date", "occupation")
+    list_filter = ( "date", "occupation")
+    ordering = ("first_name",)
+    list_per_page = 10
+    readonly_fields = ("occupation",)
+
+
+admin.site.register(Form, FormAdmin)
+
